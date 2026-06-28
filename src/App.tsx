b1c3a3613fd5fa29318c86a5d8c74f66936d8fd5
Alberto Biancardi <albertobiancardi@gmail.com>
2026-06-27 20:50:21 -0500

    Add useDebounce hook for search term optimization in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import type { Movie } from "./types";
 import MovieCard from "./components/MovieCard";
+import { useDebounce } from "react-use";
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const API_OPTIONS: RequestInit = {
@@ -17,6 +18,12 @@ export default function App() {
     const [errorMessage, setErrorMessage] = useState('');
     const [movieList, setMovieList] = useState<Movie[]>([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+    useDebounce(
+        () => {
+            setDebouncedSearchTerm(searchTerm);
+        }, 500, [searchTerm])
 
     const fetchMovies = async (query = '') => {
         setIsLoading(true);
@@ -47,8 +54,8 @@ export default function App() {
 
 
     useEffect(() => {
-        fetchMovies(searchTerm);
-    }, [searchTerm])
+        fetchMovies(debouncedSearchTerm);
+    }, [debouncedSearchTerm])
 
     return (
         <main>
